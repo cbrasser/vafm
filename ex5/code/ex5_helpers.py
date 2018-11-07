355e0cda39547ed4a15dd59e56ae1bd5fd55e8e7
ClaDos <brasser13@gmail.com>
2018-11-07 18:38:17 +0100

    i dont know why this does not work

--- ex5/code/ex5_helpers.py
+++ ex5/code/ex5_helpers.py
@@ -15,18 +15,18 @@ def get_disparity(left_img, right_img, patch_radius, min_disp, max_disp):
     r = patch_radius
     width = np.shape(left_img)[0]
     height = np.shape(left_img)[1]
+    print(height, width)
 
     for row in range(patch_radius,width-patch_radius):
         for col in range(max_disp + patch_radius,height - patch_radius):
-            left_patch = left_img[(row-r):(row+r+1), (col-r):(col+r+1)]
-            right_strip = right_img[(row-r):(row+r+1), (col-r-max_disp):(col+r-min_disp+1)]
+            left_patch = np.single(left_img[(row-r):(row+r+1), (col-r):(col+r+1)])
+            right_strip = np.single(right_img[(row-r):(row+r+1), (col-r-max_disp):(col+r-min_disp+1)])
 
-
-            left_patch_vec = left_patch.reshape((121,1))
-            right_strip_vec = np.zeros((np.power(patch_size,2), max_disp - min_disp + 1))
+            left_patch = np.single(left_patch.reshape((np.power(patch_size,2),1)))
+            right_strip_vec = np.single(np.zeros((np.power(patch_size,2), max_disp - min_disp + 1)))
             for i in range(0,patch_size):
                 right_strip_vec[((i)*patch_size):((i+1)*patch_size), :] = right_strip[:, i:(max_disp - min_disp + i + 1)]
-            ssds = cdist(np.transpose(left_patch_vec),np.transpose(right_strip_vec),metric='sqeuclidean')
+            ssds = cdist(np.transpose(left_patch),np.transpose(right_strip_vec),metric='sqeuclidean')
             min_ssd = np.argmin(ssds)
             disp_img[row, col] = max_disp - min_ssd
 
