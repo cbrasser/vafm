ad01817b07c7dbe9efb242688f59fb8b0c31eab4
Claudio Brasser <brasser13@gmail.com>
2018-12-02 12:30:14 +0100

    started exercise 8

--- ex8/code/main.py
+++ ex8/code/main.py
@@ -7,23 +7,51 @@ import util
 
 DATA = '../data'
 I_R = np.asarray(Image.open(os.path.join(DATA, '000000.png')).convert('L'))
-plt.subplot(2,2,1)
-plt.imshow(I_R)
-plt.title('Reference')
+def part_1():
+    plt.subplot(2,2,1)
+    plt.imshow(I_R)
+    plt.title('Reference')
 
-plt.subplot(2,2,2)
-W = util.get_sim_warp(50, -30, 0, 1)
-plt.imshow(util.warp_image(I_R, W))
-plt.title('Translation')
+    plt.subplot(2,2,2)
+    W = util.get_sim_warp(50, -30, 0, 1)
+    plt.imshow(util.warp_image(I_R, W))
+    plt.title('Translation')
 
-plt.subplot(2,2,3)
-W = util.get_sim_warp(0, 0, 10, 1);
-plt.imshow(util.warp_image(I_R, W))
-plt.title('Rotation around upper left corner')
+    plt.subplot(2,2,3)
+    W = util.get_sim_warp(0, 0, 10, 1);
+    plt.imshow(util.warp_image(I_R, W))
+    plt.title('Rotation around upper left corner')
 
-plt.subplot(2,2,4)
-W = util.get_sim_warp(0, 0, 0, 0.5);
-plt.imshow(util.warp_image(I_R, W))
-plt.title('Zoom on upper left corner')
+    plt.subplot(2,2,4)
+    W = util.get_sim_warp(0, 0, 0, 0.5);
+    plt.imshow(util.warp_image(I_R, W))
+    plt.title('Zoom on upper left corner')
 
-plt.show()
+    plt.show()
+
+#----------------------------Part 2--------------------------
+def part_2():
+    # Get and display template:
+    plt.subplot(1, 2, 1)
+    W0 = util.get_sim_warp(0, 0, 0, 1)
+    x_T = np.asarray([900, 291])
+    r_T = 15
+    template = util.get_warped_patch(I_R, W0, x_T, r_T)
+    plt.imshow(template)
+    # axis equal;
+    # axis off;
+    plt.title('Template')
+
+    plt.subplot(1, 2, 2)
+    W = util.get_sim_warp(10, 6, 0, 1)
+    I = util.warp_image(I_R, W)
+    r_D = 20
+    dx, ssds = util.track_brute_force(I_R, I, x_T, r_T, r_D)
+    plt.imshow(ssds)
+    # axis equal;
+    # axis off;
+    plt.title('SSDs')
+    print([f'Displacement best explained by (dx, dy) = ( {dx} )'])
+    plt.show()
+
+part_2()
--- ex8/code/util.py
+++ ex8/code/util.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.spatial.distance import cdist
+
 
 def get_sim_warp(dx, dy, alpha_deg, lam):
     '''
@@ -30,3 +32,62 @@ def warp_image(I_R, W):
 
 
     return I
+
+def get_warped_patch(I, W, x_T, r_T):
+    '''
+    params:
+    x_T is the point to track and is 1x2 and contains [x_T y_T].
+    r_T is radius of patch to track
+    returns:
+    patch is (2*r_T+1)x(2*r_T+1) and arranged consistently with the input image I.
+    '''
+    patch = np.zeros((2*r_T+1, 2*r_T+1))
+
+    for x in range(-r_T,r_T):
+        for y in range(-r_T,r_T):
+            pre_warp = np.asarray([x,y])
+            warped = x_T + np.hstack((pre_warp,1)).dot(np.transpose(W))
+            patch[y + r_T + 1, x + r_T + 1] = I[int(warped[1]), int(warped[0])]
+
+    return patch
+
+def track_brute_force(I_R, I, x_T, r_T, r_D):
+    '''
+    Params:
+    I_R: reference image
+    I: image to track point in
+    x_T: point to track, expressed as [x y]=[col row]
+    r_T: radius of patch to track
+    r_D: radius of patch to search dx within
+    returns:
+    dx: translation that best explains where x_T is in image I
+    ssds: SSDs for all values of dx within the patch defined by center x_T and radius r_D.
+
+    '''
+    dx_best = (0,0)
+    ssds = np.zeros((2*r_D, 2*r_D))
+    template = get_warped_patch(I_R, get_sim_warp(0,0,0,1), x_T, r_T)
+
+    for dx in range(-r_D,r_D):
+        for dy in range(-r_D,r_D):
+            candidate = get_warped_patch(I, get_sim_warp(dx,dy,0,1), x_T, r_T)
+            ssd = np.sum(np.sum(np.square(template - candidate)));
+            ssds[dx + r_D, dy + r_D] = ssd
+
+    index = np.unravel_index(np.argmin(ssds, axis=None), ssds.shape)
+    dx_best = np.asarray([index[0], index[1]]) - r_D - 1
+
+    return dx_best, ssds
+
+
+
+
+
+def track_KLT_robust():
+    pass
+
+def track_KLT():
+    pass
+
+def plot_matches():
+    pass
